2e29613680a1976738e9679f4df803855d37ad05
Samus5 <a_samuel.fernandez.rodriguez@iespablopicasso.es>
2023-11-22 09:38:22 +0100

    Subida de Numeros Aleatorios

---
+++ src/ed/git/act3/NumerosAleatorios.java
@@ -0,0 +1,16 @@
+package ed.git.act3;
+import java.util.Random;
+public class NumerosAleatorios {
+	
+	  public static void main(String[] args) {
+	        mostrarNumerosAleatorios();
+	    }
+	    public static void mostrarNumerosAleatorios() {
+	        Random rand = new Random();
+	        System.out.println("20 números aleatorios entre 1 y 10:");
+	        for (int i = 0; i < 20; i++) {
+	            int numeroAleatorio = rand.nextInt(10) + 1; // Genera un numero entre 1 y 10
+	            System.out.print(numeroAleatorio + " ");
+	        }
+	    }
+	}
