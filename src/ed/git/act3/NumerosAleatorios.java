ba9d5af73556a2687cd9153caf9ed70bd69589dd
Samus5 <a_samuel.fernandez.rodriguez@iespablopicasso.es>
2023-11-27 09:33:11 +0100

    30 Números en vez de 20

--- src/ed/git/act3/NumerosAleatorios.java
+++ src/ed/git/act3/NumerosAleatorios.java
@@ -7,8 +7,8 @@ public class NumerosAleatorios {
 	    }
 	    public static void mostrarNumerosAleatorios() {
 	        Random rand = new Random();
-	        System.out.println("20 números aleatorios entre 1 y 10:");
-	        for (int i = 0; i < 20; i++) {
+	        System.out.println("30 números aleatorios entre 1 y 10:");
+	        for (int i = 0; i < 30; i++) {
 	            int numeroAleatorio = rand.nextInt(10) + 1; // Genera un numero entre 1 y 10
 	            System.out.print(numeroAleatorio + " ");
 	        }
